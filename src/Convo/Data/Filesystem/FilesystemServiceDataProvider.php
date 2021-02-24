3922853264c6f5a966fd348bed7f6247c3bedd43
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-02-24 15:22:23 +0100

    Use proper owner object

--- src/Convo/Data/Filesystem/FilesystemServiceDataProvider.php
+++ src/Convo/Data/Filesystem/FilesystemServiceDataProvider.php
@@ -13,11 +13,18 @@ class FilesystemServiceDataProvider extends AbstractServiceDataProvider
 
 	private $_basePath;
 
+	/**
+	 * @var \Convo\Core\IAdminUserDataProvider
+	 */
+	private $_userDataProvider;
 
-	public function __construct( \Psr\Log\LoggerInterface $logger, $basePath)
+	public function __construct( \Psr\Log\LoggerInterface $logger, \Convo\Core\IAdminUserDataProvider $userProvider, $basePath)
 	{
 	    parent::__construct( $logger);
-		$this->_basePath	=	\Convo\Core\Util\StrUtil::removeTrailingSlashes( $basePath);
+
+		$this->_basePath = \Convo\Core\Util\StrUtil::removeTrailingSlashes( $basePath);
+
+		$this->_userDataProvider = $userProvider;
 	}
 
 	/**
@@ -47,10 +54,12 @@ class FilesystemServiceDataProvider extends AbstractServiceDataProvider
 			try {
 			    $serviceMeta = $this->getServiceMeta( $user, $service_id);
 			    if ($this->_checkServiceOwner($user, $serviceMeta)) {
+					$owner = $this->_userDataProvider->findUser($serviceMeta['owner']);
+
 					$serviceMeta['owner'] = [
-						'name' => $user->getName(),
-						'username' => $user->getUsername(),
-						'email' => $user->getEmail()
+						'name' => $owner->getName(),
+						'username' => $owner->getUsername(),
+						'email' => $owner->getEmail()
 					];
                     $all[] = $serviceMeta;
                 }
